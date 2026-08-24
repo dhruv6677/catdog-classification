6aeacb8f80d171a616b8312defc1cb9f836edf5e
dhruv <progammerdhruv@gmail.com>
2026-08-24 21:37:24 +0530

    deploy

--- app.py
+++ app.py
@@ -37,7 +37,11 @@ def predictRoute():
 
 
 # port = int(os.getenv("PORT"))
+# if __name__ == "__main__":
+#     clApp = ClientApp()
+#     # app.run(host='0.0.0.0', port=port)
+#     app.run(host='0.0.0.0', port=8000, debug=True)
+
 if __name__ == "__main__":
-    clApp = ClientApp()
-    # app.run(host='0.0.0.0', port=port)
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
